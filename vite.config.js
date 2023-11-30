ffe5a27aea4fd178a664759b79a31f22f6bb5a19
leif@synapticsoftware.net <leif@synapticsoftware.net>
2023-11-30 14:38:42 -0700

    add helperfuncs to vite config

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,10 @@ export default defineConfig(({mode}) => {
         },
         plugins: [
             laravel({
-                input: ['resources/app.jsx'],
+                input: [
+                    'resources/app.jsx',
+                    'resources/js/helperFuncs.js'
+                ],
                 refresh: true,
             }),
             react()
