60ad94edd57548e551ead12eef9e937b5b6be3ca
Leif Nervick <135361039+lcnervick@users.noreply.github.com>
2026-06-30 10:22:58 -0500

    fix contact form controller

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use ReCaptcha\ReCaptcha;
@@ -11,10 +9,8 @@ use App\Http\Controllers\MailController;
 
 class ContactFormController extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
-
     public function processForm(Request $request) {
-        $validated = $this->validate($request, [
+        $validated = $request->validate([
             'name' => 'required|min:3|max:255',
             'email' => ['required', 'email:rfc,dns,spoof', 'regex:/^[^\r\n]+$/'],
             'message'=> 'required|max:600',
